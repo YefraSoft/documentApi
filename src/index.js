212e0c5a09c12f7a8012c4bf4fc133a185b43a4a
EDUARDO EFRAIN GARCIA SUAREZ <egarcia1415@mich.conalep.edu.mx>
2023-11-24 02:17:46 -0600

    Rename files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import exp from "express";
 import proceduresRoutes from './routes/procedures.routes.js';
-import employeesRouter from './routes/teacher.routes.js';
+import TeacherCrudRouter from './routes/teacher.routes.js';
 const app = exp();
 app.listen(3000);
 app.use(exp.json());
-app.use(employeesRouter);
+app.use(TeacherCrudRouter);
 app.use(proceduresRoutes);
